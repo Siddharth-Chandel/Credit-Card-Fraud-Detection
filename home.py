420eca26a9ef5d10d2720be0a277eb7d1c5471c3
Siddharth Chandel <siddharthchandel2004@gmail.com>
2024-12-14 02:55:24 +0530

    Project documentation

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 from keras.models import load_model # type: ignore
 
-data = pd.read_csv(r"data\card_transdata.csv")
+data = pd.read_csv(r"data/card_transdata.csv")
 st.title("Credit Card Fraud Detection")
 st.write("This is a simple web application that uses a machine learning model to detect credit card fraud using FNN (Forward neural networks).")
 st.write("The model is trained on a dataset of credit card transactions and can be used to classify different credit card transactions as fraudlent or not.")
@@ -22,19 +22,19 @@ st.write("- The dataset contains 1,000,000 rows and 7 columns.")
 with st.expander("Correlation Matrix"):
     st.write("The feature correlation heatmap shows the correlation between different features in the dataset.")
     st.write("The heatmap shows that the 'Amount' feature is highly correlated with the 'Time'")
-st.image(Image.open(r"img\feature_correlation_heatmap.png"))
+st.image(Image.open(r"img/feature_correlation_heatmap.png"))
 
 with st.expander("Class Distributionst Before SMOTE"):
     st.write("The class distribution of the dataset is as follows:")
     st.write("The fraud distribution before SMOTE is shown in the above plot.")
     st.write("The plot shows that the fraud class is highly imbalanced")
     st.write("The dataset is imbalanced with 0.17% fraud transactions.")
-st.image(Image.open(r"img\fraud_distribution_before_smote.png"))
+st.image(Image.open(r"img/fraud_distribution_before_smote.png"))
 
 with st.expander("Class Distributionst After SMOTE"):
     st.write("The fraud distribution after SMOTE is shown in the above plot.")
     st.write("The plot shows that the fraud class is now more balanced")
-st.image(Image.open(r"img\fraud_distribution_after_smote.png"))
+st.image(Image.open(r"img/fraud_distribution_after_smote.png"))
 
 st.header('Evaluation')
 st.write("The model is evaluated using the following metrics:")
@@ -42,15 +42,15 @@ st.write("1. Accuracy and loss curve during training\n2. Confusion matrix\n3. Cl
 
 st.subheader("1. Accuracy and loss curve during training")
 st.write("These curves show the logs for the training and helps to identify training patterns.")
-st.image(Image.open(r"img\training_plot.png"))
+st.image(Image.open(r"img/training_plot.png"))
 
 st.subheader("2. Confusion matrix")
 st.write("The confusion matrix is a table that is used to evaluate the performance of a classification model. It is pictorial representation of true positive, true negative, false positive and false negative.")
-st.image(Image.open(r"img\confusion_matrix.png"))
+st.image(Image.open(r"img/confusion_matrix.png"))
 
 st.subheader("3. Classification report")
 st.write("The classification report is a performance evaluation metric that is used to assess the quality of predictions from a classification algorithm. The metrics computed in the classification report are crucial for understanding the effectiveness of the prediction model, especially in the context of imbalanced datasets where the accuracy score alone can be misleading.")
-st.image(Image.open(r"img\classification_report.png"))
+st.image(Image.open(r"img/classification_report.png"))
 
 st.header("Classification on random data values")
 st.write("Distance from home")
@@ -96,7 +96,7 @@ testing_data = np.array([[dist_house, dist_last, ratio, cov(retailer), cov(chip)
 submit = st.button("Submit")
 if submit:
     with st.status("Processing"):
-        model = load_model(r"model\detector.keras")
+        model = load_model(r"model/detector.keras")
         pred = np.round(model.predict(testing_data))
     st.subheader(f"Fraud: {True if pred else False}")
 else:
